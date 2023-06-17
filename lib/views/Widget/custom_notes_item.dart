ceaa881ce45e45ae3703bb0dd5b131becab64fa4
Myproject <mohamed.alaa@gmail.com>
2023-06-17 14:00:03 -0700

    add floating action button

--- lib/views/Widget/custom_notes_item.dart
+++ lib/views/Widget/custom_notes_item.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -7,29 +6,41 @@ class NotesItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16) ,
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
       decoration: BoxDecoration(
         color: const Color(0xffFFCC80),
         borderRadius: BorderRadius.circular(16),
-
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           ListTile(
-            title:const  Text('Flutter ',style:TextStyle(color: Colors.black,fontSize: 26)),
+            title: const Text('Flutter ',
+                style: TextStyle(color: Colors.black, fontSize: 26)),
             subtitle: Padding(
               padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text('Build your carer with tharwat samy',style:TextStyle(color: Colors.black.withOpacity(0.5),fontSize: 19),),
+              child: Text(
+                'Build your carer with tharwat samy',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.5), fontSize: 18),
+              ),
             ),
-            trailing: IconButton(onPressed: (){},icon: const Icon(FontAwesomeIcons.trash,size: 30,),color: Colors.black,),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.trash,
+                size: 30,
+              ),
+              color: Colors.black,
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(right: 24),
-            child: Text('21may ,2022',style:TextStyle(color: Colors.black.withOpacity(0.4),fontSize: 16)),
+            child: Text('21may ,2022',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.4), fontSize: 16)),
           ),
-
         ],
       ),
     );
