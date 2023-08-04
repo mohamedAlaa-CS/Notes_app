56448fc1833e3dca2a5b2f78a071196a8c48a86e
Myproject <mohamed.alaa@gmail.com>
2023-08-04 00:01:02 -0700

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String tiltle;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  NoteModel({
+    required this.tiltle,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
+}
